196dc837fe7c4fec6e521eb765bdf0c2c2f435f7
Rob van Geloven <rob@vangeloven.net>
2018-04-24 15:11:37 +0200

    Initial commit

---
+++ UnitTests/VINTests.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using VINfo;
+using Xunit;
+
+namespace UnitTests
+{
+    public class VINTests
+    {
+        public static IEnumerable<object[]> ValidVINS =>
+            new List<object[]>
+        {
+                new object[] { "1ZVHT82H485113456" },
+                new object[] { "11111111111111111" }
+        };
+
+        public static IEnumerable<object[]> InvalidVINS =>
+            new List<object[]>
+        {
+                new object[] { null },
+                new object[] { "" },
+                new object[] { "IZVHT82H485113456" },
+                new object[] { "OZVHT82H485113456" },
+                new object[] { "QZVHT82H485113456" },
+                new object[] { "1ZVHT82H485113456X" },
+                new object[] { "1ZVHT82H48511345" }
+        };
+
+        [Theory]
+        [MemberData(nameof(ValidVINS))]
+        public void Valid_VIN_Validates(string vin)
+        {
+            Assert.True(VINData.IsValid(vin));
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidVINS))]
+        public void Invalid_VIN_Does_Not_Validate(string vin)
+        {
+            Assert.False(VINData.IsValid(vin));
+        }
+    }
+}
---
+++ VINfo.net/VINData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace VINfo
+{
+    public class VINData
+    {
+        public string OriginalVIN { get; }
+
+        private VINData(string vin)
+        {
+
+            OriginalVIN = vin;
+        }
+
+        private static int Transliterate(char c)
+        {
+            return "0123456789.ABCDEFGH..JKLMN.P.R..STUVWXYZ".IndexOf(c) % 10;
+        }
+
+        private static char GetCheckDigit(string vin)
+        {
+            string map = "0123456789X";
+            int[] weights = { 8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int sum = 0;
+
+            for (int i = 0; i < 17; ++i)
+            {
+                sum += Transliterate(vin[i]) * weights[i];
+            }
+
+            return map[sum % 11];
+        }
+
+        public static bool IsValid(string vin)
+        {
+            if (vin?.Length != 17) return false;
+
+            return GetCheckDigit(vin) == vin[8];
+        }
+
+        public static VINData Parse(string vin)
+        {
+            if (!IsValid(vin)) throw new ArgumentException($"VIN '{vin}' is not a valid VIN", nameof(vin));
+
+            return new VINData(vin);
+        }
+    }
+}
